bc224ec75de5ab679d1e868be846c84ee23ba1c7
Folyd <lyshuhow@gmail.com>
2024-06-09 01:08:12 -0700

    Fix search_core_js declaration

--- src/librustdoc/html/static_files.rs
+++ src/librustdoc/html/static_files.rs
@@ -95,6 +95,7 @@ static_files! {
     normalize_css => "static/css/normalize.css",
     main_js => "static/js/main.js",
     search_js => "static/js/search.js",
+    search_core_js => "static/js/search-core.js",
     settings_js => "static/js/settings.js",
     src_script_js => "static/js/src-script.js",
     storage_js => "static/js/storage.js",
